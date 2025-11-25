a61aa172f4d347a6db484b6946d195c90cfa1690
htmujahid <htmujahid@gmail.com>
2025-11-25 17:48:06 +0500

    feat: update layout title with SVG logo and add logo.svg file

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -13,10 +13,13 @@ export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: (
-        <>
-          <Grid2X2PlusIcon className="inline h-4 w-4" />
-          Supasheet
-        </>
+        <div className="flex w-fit items-center gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M12 3v17a1 1 0 0 1-1 1H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v6a1 1 0 0 1-1 1H3" />
+            <path d="M21 17v2a2 2 0 0 1-2 2h-2" />
+          </svg>
+          <span>Supasheet.</span>
+        </div>
       ),
     },
     githubUrl: siteConfig.links.github,
